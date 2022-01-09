3f12676224da96a2439fa561568c8d7e4348d556
VietnamHub <95493150+VietnamHub@users.noreply.github.com>
2022-01-09 18:33:32 +0700

    Update Ping.py

--- Yukki/Plugins/Ping.py
+++ Yukki/Plugins/Ping.py
@@ -38,7 +38,7 @@ async def ping(_, message):
     )
     uptime = await bot_sys_stats()
     end = datetime.now()
-    resp = (end - start).microseconds / 100
+    resp = (end - start).microseconds / 10
     await response.edit_text(
         f"**Pong!**\n`⚡{resp} ms`\n\n<b><u>{MUSIC_BOT_NAME} System Stats:</u></b>{uptime}"
     )
