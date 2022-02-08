1b3bf040adf3af9c5571180a5321666ee3e2437f
VietnamHub <95493150+VietnamHub@users.noreply.github.com>
2022-02-08 13:11:02 -0800

    Update Voicechat.py

--- Yukki/Plugins/Voicechat.py
+++ Yukki/Plugins/Voicechat.py
@@ -13,29 +13,26 @@ from Yukki import SUDOERS, app, db_mem, random_assistant
 from Yukki.Database import (get_active_chats, get_active_video_chats,
                             get_assistant, is_active_chat, save_assistant)
 from Yukki.Decorators.checker import checker, checkerCB
-from Yukki.Inline import primary_markup
+from Yukki.Inline import choose_markup, primary_markup
 from Yukki.Utilities.assistant import get_assistant_details
 
 loop = asyncio.get_event_loop()
 
-__MODULE__ = "Tham gia/Rời khỏi"
-__HELP__ = """
 
-**Note:**
-Chỉ dành cho người dùng Sudo
-
-
-/joinassistant [Chat Username or Chat ID]
-- Tham gia trợ lý vào một nhóm.
-
-
-/leaveassistant [Chat Username or Chat ID]
-- Trợ lý sẽ rời khỏi nhóm cụ thể.
-
-
-/leavebot [Chat Username or Chat ID]
-- Bot sẽ rời khỏi cuộc trò chuyện cụ thể.
-"""
+@app.on_callback_query(filters.regex("gback_list_chose_stream"))
+async def gback_list_chose_stream(_, CallbackQuery):
+    await CallbackQuery.answer()
+    callback_data = CallbackQuery.data.strip()
+    callback_request = callback_data.split(None, 1)[1]
+    videoid, duration, user_id = callback_request.split("|")
+    if CallbackQuery.from_user.id != int(user_id):
+        return await CallbackQuery.answer(
+            "This is not for you! Search You Own Song.", show_alert=True
+        )
+    buttons = choose_markup(videoid, duration, user_id)
+    await CallbackQuery.edit_message_reply_markup(
+        reply_markup=InlineKeyboardMarkup(buttons)
+    )
 
 
 @app.on_callback_query(filters.regex("pr_go_back_timer"))
@@ -64,13 +61,13 @@ async def timer_checkup_markup(_, CallbackQuery):
             dur_left = db_mem[CallbackQuery.message.chat.id]["left"]
             duration_min = db_mem[CallbackQuery.message.chat.id]["total"]
             return await CallbackQuery.answer(
-                f"Còn lại {dur_left} ra khỏi {duration_min} phút.",
+                f"Remaining {dur_left} out of {duration_min} Mins.",
                 show_alert=True,
             )
-        return await CallbackQuery.answer(f"Không chơi.", show_alert=True)
+        return await CallbackQuery.answer(f"Not Playing.", show_alert=True)
     else:
         return await CallbackQuery.answer(
-            f"Không có cuộc trò chuyện thoại đang hoạt động", show_alert=True
+            f"No Active Voice Chat", show_alert=True
         )
 
 
@@ -78,12 +75,12 @@ async def timer_checkup_markup(_, CallbackQuery):
 async def activevc(_, message: Message):
     global get_queue
     if await is_active_chat(message.chat.id):
-        mystic = await message.reply_text("Vui lòng đợi ... Đang xếp hàng ..")
+        mystic = await message.reply_text("Please Wait... Getting Queue..")
         dur_left = db_mem[message.chat.id]["left"]
         duration_min = db_mem[message.chat.id]["total"]
         got_queue = get_queue.get(message.chat.id)
         if not got_queue:
-            await mystic.edit(f"Không có gì trong hàng đợi")
+            await mystic.edit(f"Nothing in Queue")
         fetched = []
         for get in got_queue:
             fetched.append(get)
@@ -92,22 +89,22 @@ async def activevc(_, message: Message):
         current_playing = fetched[0][0]
         user_name = fetched[0][1]
 
-        msg = "**Danh sách xếp hàng**\n\n"
-        msg += "**Hiện đang chơi:**"
+        msg = "**Queued List**\n\n"
+        msg += "**Currently Playing:**"
         msg += "\n▶️" + current_playing[:30]
         msg += f"\n   ╚By:- {user_name}"
-        msg += f"\n   ╚Thời lượng:- Còn lại `{dur_left}` ra khỏi `{duration_min}` phút."
+        msg += f"\n   ╚Duration:- Remaining `{dur_left}` out of `{duration_min}` Mins."
         fetched.pop(0)
         if fetched:
             msg += "\n\n"
-            msg += "**Tiếp theo trong hàng đợi:**"
+            msg += "**Up Next In Queue:**"
             for song in fetched:
                 name = song[0][:30]
                 usr = song[1]
                 dur = song[2]
                 msg += f"\n⏸️{name}"
-                msg += f"\n   ╠Thời lượng : {dur}"
-                msg += f"\n   ╚Được yêu cầu bởi : {usr}\n"
+                msg += f"\n   ╠Duration : {dur}"
+                msg += f"\n   ╚Requested by : {usr}\n"
         if len(msg) > 4096:
             await mystic.delete()
             filename = "queue.txt"
@@ -115,14 +112,14 @@ async def activevc(_, message: Message):
                 out_file.write(str(msg.strip()))
             await message.reply_document(
                 document=filename,
-                caption=f"**OUTPUT:**\n\n`Danh sách xếp hàng`",
+                caption=f"**OUTPUT:**\n\n`Queued List`",
                 quote=False,
             )
             os.remove(filename)
         else:
             await mystic.edit(msg)
     else:
-        await message.reply_text(f"Không có gì trong hàng đợi")
+        await message.reply_text(f"Nothing in Queue")
 
 
 @app.on_message(filters.command("activevc") & filters.user(SUDOERS))
@@ -150,10 +147,10 @@ async def activevc(_, message: Message):
             text += f"<b>{j + 1}. {title}</b> [`{x}`]\n"
         j += 1
     if not text:
-        await message.reply_text("Không có cuộc trò chuyện thoại nào đang hoạt động")
+        await message.reply_text("No Active Voice Chats")
     else:
         await message.reply_text(
-            f"**Trò chuyện thoại Hoạt động:-**\n\n{text}",
+            f"**Active Voice Chats:-**\n\n{text}",
             disable_web_page_preview=True,
         )
 
@@ -183,10 +180,10 @@ async def activevi_(_, message: Message):
             text += f"<b>{j + 1}. {title}</b> [`{x}`]\n"
         j += 1
     if not text:
-        await message.reply_text("Không có cuộc trò chuyện thoại nào đang hoạt động")
+        await message.reply_text("No Active Voice Chats")
     else:
         await message.reply_text(
-            f"**Cuộc gọi điện video đang hoạt động:-**\n\n{text}",
+            f"**Active Video Calls:-**\n\n{text}",
             disable_web_page_preview=True,
         )
 
@@ -195,7 +192,7 @@ async def activevi_(_, message: Message):
 async def basffy(_, message):
     if len(message.command) != 2:
         await message.reply_text(
-            "**Cách sử dụng:**\n/joinassistant [Chat Username or Chat ID]"
+            "**Usage:**\n/joinassistant [Chat Username or Chat ID]"
         )
         return
     chat = message.text.split(None, 2)[1]
@@ -203,12 +200,12 @@ async def basffy(_, message):
         chat_id = (await app.get_chat(chat)).id
     except:
         return await message.reply_text(
-            "Thêm Bot vào Trò chuyện này Đầu tiên .. Trò chuyện không xác định cho bot"
+            "Add Bot to this Chat First.. Unknown Chat for the bot"
         )
     _assistant = await get_assistant(chat_id, "assistant")
     if not _assistant:
         return await message.reply_text(
-            "Không tìm thấy Trợ lý được lưu trước.\n\nBạn có thể đặt Trợ lý qua /play bên trong Nhóm của {Chat}"
+            "No Pre-Saved Assistant Found.\n\nYou can set Assistant Via /play inside {Chat}'s Group"
         )
     else:
         ran_ass = _assistant["saveassistant"]
@@ -218,7 +215,7 @@ async def basffy(_, message):
     try:
         await ASS_ACC.join_chat(chat_id)
     except Exception as e:
-        await message.reply_text(f"Failed\n**Lý do có thể có thể là**:{e}")
+        await message.reply_text(f"Failed\n**Possible reason could be**:{e}")
         return
     await message.reply_text("Joined.")
 
@@ -227,24 +224,24 @@ async def basffy(_, message):
 async def baaaf(_, message):
     if len(message.command) != 2:
         await message.reply_text(
-            "**Cách sử dụng:**\n/leavebot [Chat Username or Chat ID]"
+            "**Usage:**\n/leavebot [Chat Username or Chat ID]"
         )
         return
     chat = message.text.split(None, 2)[1]
     try:
         await app.leave_chat(chat)
     except Exception as e:
-        await message.reply_text(f"Failed\n**Lý do có thể có thể là**:{e}")
+        await message.reply_text(f"Failed\n**Possible reason could be**:{e}")
         print(e)
         return
-    await message.reply_text("Bot đã rời khỏi cuộc trò chuyện thành công")
+    await message.reply_text("Bot has left the chat successfully")
 
 
 @app.on_message(filters.command("leaveassistant") & filters.user(SUDOERS))
 async def baujaf(_, message):
     if len(message.command) != 2:
         await message.reply_text(
-            "**Cách sử dụng:**\n/leave [Username hoặc ID nhóm]"
+            "**Usage:**\n/leave [Chat Username or Chat ID]"
         )
         return
     chat = message.text.split(None, 2)[1]
@@ -252,12 +249,12 @@ async def baujaf(_, message):
         chat_id = (await app.get_chat(chat)).id
     except:
         return await message.reply_text(
-            "Thêm Bot vào Trò chuyện này Đầu tiên .. Trò chuyện không xác định cho bot"
+            "Add Bot to this Chat First.. Unknown Chat for the bot"
         )
     _assistant = await get_assistant(chat, "assistant")
     if not _assistant:
         return await message.reply_text(
-            "Không tìm thấy Trợ lý được lưu trước.\n\nBạn có thể đặt Trợ lý qua /play trong Nhóm của {Chat}"
+            "No Pre-Saved Assistant Found.\n\nYou can set Assistant Via /play inside {Chat}'s Group"
         )
     else:
         ran_ass = _assistant["saveassistant"]
@@ -267,6 +264,6 @@ async def baujaf(_, message):
     try:
         await ASS_ACC.leave_chat(chat_id)
     except Exception as e:
-        await message.reply_text(f"Failed\n**Bot đã rời khỏi cuộc trò chuyện thành công**:{e}")
+        await message.reply_text(f"Failed\n**Possible reason could be**:{e}")
         return
-    await message.reply_text("Rời nhóm.")
+    await message.reply_text("Left.")
